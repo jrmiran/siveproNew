15c6db6165ac4c7907e89333d3c934d1311b2411
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-12 14:15:22 -0300

    make it better

--- src/app/comp/comp.component.ts
+++ src/app/comp/comp.component.ts
@@ -18,6 +18,34 @@ export class CompComponent implements OnInit{
     peopleVector:  PeopleModel[] = [];
     
     
+    makeClients(){
+         var self = this;
+           this.appService.searchAllPeople().subscribe(function(data){
+                console.log(data);
+                data.forEach(function(value){
+                    self.peopleType = Object.assign(self.peopleType, value);
+                    self.peopleVector.push(self.peopleType);
+                    
+                    self.peopleType = {DTYPE: '', bairro: '', celular: '', celular2: '', cidade: '', cnpj: '', complemento: '', cpf: '', email: '', endereco: '', enderecoObra: '', id: 0, nome: '', telefone: '', telefone2: ''};
+                });
+               console.log(self.peopleVector);
+               self.peopleVector.forEach(function(value){
+                   self.appService.clientStoreInsertion(value.bairro,
+                    value.celular,
+                    value.celular2,
+                    value.cidade,
+                    value.complemento,
+                    value.email,
+                    value.endereco,
+                    value.nome,
+                    value.telefone,
+                    value.telefone2,
+                    "Belartte",).subscribe(function(data){
+                     console.log(data); 
+                  });
+               });
+           });
+    }
     ngOnInit() {
             /*var self = this;
            this.appService.searchAllPeople().subscribe(function(data){
